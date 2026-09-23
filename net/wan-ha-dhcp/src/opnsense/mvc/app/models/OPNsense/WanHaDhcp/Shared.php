9f73082e73470fc3d1c1469a9aba06ebaffe56a5
MirariImmensitatemAstrorum <bryan@bwitpro.com>
2026-09-22 21:03:50 -0500

    wan-ha-dhcp: validate enabled shared configuration

--- net/wan-ha-dhcp/src/opnsense/mvc/app/models/OPNsense/WanHaDhcp/Shared.php
+++ net/wan-ha-dhcp/src/opnsense/mvc/app/models/OPNsense/WanHaDhcp/Shared.php
@@ -3,7 +3,75 @@
 namespace OPNsense\WanHaDhcp;
 
 use OPNsense\Base\BaseModel;
+use OPNsense\Base\Messages\Message;
+use OPNsense\Core\Config;
 
 class Shared extends BaseModel
 {
+    public function performValidation($validateFullModel = false)
+    {
+        $messages = parent::performValidation($validateFullModel);
+
+        if (empty((string)$this->enabled)) {
+            return $messages;
+        }
+
+        $mac = strtolower(trim((string)$this->shared_mac));
+        if (empty($mac)) {
+            $messages->appendMessage(new Message(
+                gettext('A shared WAN MAC is required when WAN HA DHCP is enabled.'),
+                $this->shared_mac->getInternalXMLTagName()
+            ));
+        } elseif (filter_var($mac, FILTER_VALIDATE_MAC)) {
+            $octets = array_map('hexdec', explode(':', $mac));
+            if ($mac === '00:00:00:00:00:00' || $mac === 'ff:ff:ff:ff:ff:ff' || ($octets[0] & 0x01)) {
+                $messages->appendMessage(new Message(
+                    gettext('The shared WAN MAC must be a usable unicast address.'),
+                    $this->shared_mac->getInternalXMLTagName()
+                ));
+            }
+        }
+
+        $interface = (string)$this->managed_interface;
+        $config = Config::getInstance()->object();
+        if (empty($config->interfaces->$interface)) {
+            $messages->appendMessage(new Message(
+                gettext('The managed interface does not exist.'),
+                $this->managed_interface->getInternalXMLTagName()
+            ));
+            return $messages;
+        }
+
+        $managed = $config->interfaces->$interface;
+        if ((string)$managed->ipaddr !== 'dhcp') {
+            $messages->appendMessage(new Message(
+                gettext('Version 1 requires the managed interface to use IPv4 DHCP.'),
+                $this->managed_interface->getInternalXMLTagName()
+            ));
+        }
+
+        $ipv6 = strtolower(trim((string)$managed->ipaddrv6));
+        if (!empty($ipv6) && $ipv6 !== 'none') {
+            $messages->appendMessage(new Message(
+                gettext('IPv6 on the managed WAN is not supported by version 1.'),
+                $this->managed_interface->getInternalXMLTagName()
+            ));
+        }
+
+        if ((string)$managed->if !== 'wanha0') {
+            $messages->appendMessage(new Message(
+                gettext('The managed interface must be assigned to wanha0 before WAN HA DHCP can be enabled.'),
+                $this->managed_interface->getInternalXMLTagName()
+            ));
+        }
+
+        if (!empty((string)$managed->spoofmac)) {
+            $messages->appendMessage(new Message(
+                gettext('Remove the native interface MAC spoof setting and use the plugin Shared WAN MAC instead.'),
+                $this->shared_mac->getInternalXMLTagName()
+            ));
+        }
+
+        return $messages;
+    }
 }
